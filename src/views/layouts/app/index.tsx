cf10d6de1ebe46e40e35d823b6ce3130419d5be8
kou-pg-0131 <kou.pg.0131@gmail.com>
2020-06-28 22:04:41 +0900

    CssBaseline を追加

--- src/views/layouts/app/index.tsx
+++ src/views/layouts/app/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { CssBaseline } from '@material-ui/core';
 
 import { Header } from '../header';
 import { Main } from '../main';
@@ -7,6 +8,7 @@ import { Footer } from '../footer';
 export const App: React.FC = () => {
   return (
     <div>
+      <CssBaseline/>
       <Header/>
       <Main/>
       <Footer/>
