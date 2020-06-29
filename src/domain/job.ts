0e0c70b67d954e6abb29ba3f09145c2529b64447
kou-pg-0131 <kou.pg.0131@gmail.com>
2020-06-29 22:27:00 +0900

    リファクタ

--- src/domain/job.ts
+++ src/domain/job.ts
@@ -2,7 +2,7 @@ import { Website } from './website';
 import { Language } from './language';
 
 export type Job = {
-  date: Date;
+  date: string;
   language: Language;
   website: Website;
   count: number;
--- src/views/pages/jobs/chart/index.tsx
+++ src/views/pages/jobs/chart/index.tsx
@@ -1,11 +1,9 @@
-import React, { useState } from 'react';
-import { Box, CircularProgress } from '@material-ui/core';
+import React from 'react';
+import { Box } from '@material-ui/core';
 import { makeStyles, createStyles } from '@material-ui/core/styles';
 import { Job } from '../../../../domain/job';
 import { Website } from '../../../../domain/website';
 import { Language } from '../../../../domain/language';
-import { Websites } from './websites';
-import { Sort } from './sort';
 import {
   BarChart,
   Bar,
@@ -16,11 +14,6 @@ import {
   CartesianGrid,
   ResponsiveContainer
 } from 'recharts';
-
-type Props = {
-  fetched: boolean;
-  jobs: Job[];
-};
 
 const useStyles = makeStyles(() =>
   createStyles({
@@ -40,34 +33,27 @@ const useStyles = makeStyles(() =>
   })
 );
 
+type Props = {
+  website: 'all' | Website;
+  jobs: Job[];
+  sort: boolean;
+};
+
 export const Chart: React.FC<Props> = (props: Props) => {
   const classes = useStyles();
 
-  const [sort, setSort] = useState<boolean>(false);
-  const [website, setWebsite] = useState<'all' | Website>('all');
-
-  const handleChangeSort = (sort: boolean) => setSort(sort);
-  const handleChangeWebsite = (website: 'all' | Website) => setWebsite(website);
-
-  const websites: Website[] = props.jobs.map(job => job.website).filter((website, i, self) =>
-    self.findIndex((w) => website.name === w.name) === i
-  );
-
   const map = new Map<Language, number>([]);
-  props.jobs.filter(job => website === 'all' || job.website.name === website.name).forEach(job => {
+  props.jobs.filter(job => props.website === 'all' || job.website.name === props.website.name).forEach(job => {
     map.set(job.language, (map.get(job.language) || 0) + job.count);
   });
 
   const data = (() => {
     const rows = Array.from(map).map(([language, count]) => ({ name: language, count: count }));
-    return sort ? rows.sort((a, b) => b.count - a.count) : rows;
+    return props.sort ? rows.sort((a, b) => b.count - a.count) : rows;
   })();
 
   return (
-    <Box style={{ opacity: props.fetched ? 1 : 0.5, pointerEvents: props.fetched ? 'auto' : 'none' }} className={classes.root}>
-      {props.fetched ? null : <Box className={classes.circle}><CircularProgress/></Box>}
-      <Websites websites={websites} onChange={handleChangeWebsite}/>
-      <Sort onChange={handleChangeSort}/>
+    <Box className={classes.root}>
       <ResponsiveContainer height={450}>
         <BarChart
           layout='vertical'
