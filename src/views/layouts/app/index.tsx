4814d645212ed4187a032efc9cb7fffa0dbb3c37
kou-pg-0131 <kou.pg.0131@gmail.com>
2020-07-01 21:28:10 +0900

    スタイルを調整

--- src/views/layouts/app/index.tsx
+++ src/views/layouts/app/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { CssBaseline, Box } from '@material-ui/core';
-import { createStyles, makeStyles } from '@material-ui/core/styles';
+import { ThemeProvider, createStyles, makeStyles } from '@material-ui/core/styles';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
@@ -8,6 +8,7 @@ import { createStore } from '../../modules/store';
 import { Header } from '../header';
 import { Main } from '../main';
 import { Footer } from '../footer';
+import { customTheme } from './customTheme';
 
 const useStyles = makeStyles(() =>
   createStyles({
@@ -27,14 +28,16 @@ export const App: React.FC = () => {
 
   return (
     <Provider store={store}>
-      <CssBaseline/>
-      <Box className={classes.root}>
-        <Router history={history}>
-          <Header/>
-          <Main/>
-          <Footer/>
-        </Router>
-      </Box>
+      <ThemeProvider theme={customTheme}>
+        <CssBaseline/>
+        <Box className={classes.root}>
+          <Router history={history}>
+            <Header/>
+            <Main/>
+            <Footer/>
+          </Router>
+        </Box>
+      </ThemeProvider>
     </Provider>
   );
 };
--- src/views/layouts/footer/index.tsx
+++ src/views/layouts/footer/index.tsx
@@ -5,7 +5,10 @@ import { createStyles, makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles(() =>
   createStyles({
     root: {
+      backgroundColor: '#0D1321',
+      boxShadow: '0px -2px 4px -1px rgba(0,0,0,0.2), 0px -4px 5px 0px rgba(0,0,0,0.14), 0px -1px 10px 0px rgba(0,0,0,0.12)',
       bottom: 0,
+      color: '#fff',
       height: 60,
       left: 0,
       padding: 8,
