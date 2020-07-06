f1b91e1985128e01d6aa7afb076a3b2ee2dd3d8c
kou <47014617+kou-pg-0131@users.noreply.github.com>
2020-07-06 21:55:57 +0900

    テーブルを見やすくした (#16)

--- src/views/pages/jobs/languagesTable/index.tsx
+++ src/views/pages/jobs/languagesTable/index.tsx
@@ -1,7 +1,37 @@
 import React from 'react';
 import { Box, Link, TableContainer, Table, TableHead, TableBody, TableRow, TableCell, Paper } from '@material-ui/core';
+import { makeStyles, createStyles, withStyles } from '@material-ui/core/styles';
+import { grey } from '@material-ui/core/colors';
 import { Launch as LaunchIcon } from '@material-ui/icons';
 import { Language, languageToString, Website } from '../../../../domain';
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    root: {
+      margin: '0 auto',
+      maxWidth: 500,
+    },
+    dataRow: {
+      '&:hover': {
+        background: grey[300],
+      },
+    },
+  })
+);
+
+const StyledTableCell = withStyles(()=>
+  createStyles({
+    head: {
+      backgroundColor: '#333',
+      color: '#fff',
+      fontSize: 16,
+      fontWeight: 'bold',
+    },
+    body: {
+      fontSize: 14,
+    },
+  })
+)(TableCell);
 
 type Props = {
   languages: { name: Language; count: number; searchUrl: string }[];
@@ -9,21 +39,23 @@ type Props = {
 };
 
 export const LanguagesTable: React.FC<Props> = (props: Props) => {
+  const classes = useStyles();
+
   return (
-    <TableContainer component={Paper}>
+    <TableContainer component={Paper} className={classes.root}>
       <Table stickyHeader>
         <TableHead>
           <TableRow>
-            <TableCell align='center'>順位</TableCell>
-            <TableCell align='center'>言語</TableCell>
-            <TableCell align='center'>求人数</TableCell>
+            <StyledTableCell align='center'>順位</StyledTableCell>
+            <StyledTableCell align='center'>言語</StyledTableCell>
+            <StyledTableCell align='center'>求人数</StyledTableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {props.languages.sort((a, b) => b.count - a.count).map((language, i) =>
-            <TableRow key={i}>
-              <TableCell align='center'>{i + 1}</TableCell>
-              <TableCell align='center'>
+            <TableRow key={i} className={classes.dataRow}>
+              <StyledTableCell align='center'>{i + 1}</StyledTableCell>
+              <StyledTableCell align='center'>
                 {languageToString(language.name)}
                 {props.website === 'all' ? null : (
                   <Box>
@@ -35,8 +67,8 @@ export const LanguagesTable: React.FC<Props> = (props: Props) => {
                     </Box>
                   </Box>
                 )}
-              </TableCell>
-              <TableCell align='center'>{language.count}</TableCell>
+              </StyledTableCell>
+              <StyledTableCell align='center'>{language.count.toLocaleString()}</StyledTableCell>
             </TableRow>
           )}
         </TableBody>
