de7c7fdeff57c0cdca08d79940beec17a2d15668
kou-pg-0131 <kou.pg.0131@gmail.com>
2021-02-14 17:18:22 +0900

    variables.tf を整理

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,10 +1,5 @@
-variable "stage" { default = "prod" }
-variable "basic_auth_username" {}
-variable "basic_auth_password" {}
-
 locals {
-  zone_name  = "kou-pg.com"
-  sub_domain = var.stage == "prod" ? "" : "${var.stage}."
-  domain     = "${local.sub_domain}nojov.${local.zone_name}"
-  prefix     = "nojov-ui-${var.stage}"
+  zone_name = "kou-pg.com"
+  domain    = "nojov.${local.zone_name}"
+  prefix    = "nojov-ui"
 }
