5ee96a6ca9e82abab9ebb016ecc143f225b7655a
kou-pg-0131 <kou.pg.0131@gmail.com>
2021-02-14 15:07:12 +0900

    REACT_APP -> NEXT_PUBLIC

--- src/infrastructures/googleAnalytics.ts
+++ src/infrastructures/googleAnalytics.ts
@@ -1,10 +1,10 @@
 export class GoogleAnalytics {
   constructor(
-    private measurementId: string = process.env.REACT_APP_GA_ID,
-    private debug: boolean = process.env.REACT_APP_STAGE !== 'prod',
+    private measurementId: string = process.env.NEXT_PUBLIC_GA_ID,
+    private debug: boolean = process.env.NEXT_PUBLIC_APP_STAGE !== 'prod',
   ) {}
 
-  public pageView(pathname: string): void {
+  public pageView(_pathname: string): void {
     // window.gtag('config', this.measurementId, { page_path: pathname, debug_mode: this.debug });
   }
 }
--- src/infrastructures/nojovAPIClientFactory.ts
+++ src/infrastructures/nojovAPIClientFactory.ts
@@ -2,6 +2,6 @@ import { NojovAPIClient, INojovAPIClient } from '.';
 
 export class NojovAPIClientFactory {
   public create(): INojovAPIClient {
-    return new NojovAPIClient('https://dev.api.nojov.kou-pg.com');
+    return new NojovAPIClient(process.env.NEXT_PUBLIC_API_ORIGIN);
   }
 }
