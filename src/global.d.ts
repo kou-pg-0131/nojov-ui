34a35357dc3b4ec92d52e111f3038cc754c62ecb
kou-pg-0131 <kou.pg.0131@gmail.com>
2021-02-14 22:01:55 +0900

    GA を再度有効化

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,6 +1,6 @@
 declare namespace NodeJS {
   interface ProcessEnv {
-    readonly NEXT_PUBLIC_APP_STAGE: 'dev' | 'prod';
+    readonly NEXT_PUBLIC_APP_STAGE?: 'prod';
     readonly NEXT_PUBLIC_API_ORIGIN: string;
     readonly NEXT_PUBLIC_GA_ID: string;
   }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import { AppProps } from 'next/app';
 import { CssBaseline, createMuiTheme, ThemeProvider } from '@material-ui/core';
 import { JobsProvider } from '../contexts';
@@ -14,10 +15,17 @@ const theme = createMuiTheme({
 });
 
 const App: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter();
+
   useEffect(() => {
     const jssStyles = document.getElementById('jss-server-side');
     if (jssStyles) jssStyles.remove();
   }, []);
+
+  useEffect(() => {
+    if (process.env.NEXT_PUBLIC_APP_STAGE !== 'prod') return;
+    window.gtag('config', process.env.NEXT_PUBLIC_GA_ID, { page_path: router.pathname });
+  }, [router.pathname]);
 
   return (
     <JobsProvider>
