ee70c649ead311a20a13479e3f5d11c8b5d8f65d
kou-pg-0131 <kou.pg.0131@gmail.com>
2021-02-14 14:40:13 +0900

    求人数一覧を表示するところまで

--- src/infrastructures/nojovAPIClientFactory.ts
+++ src/infrastructures/nojovAPIClientFactory.ts
@@ -2,6 +2,6 @@ import { NojovAPIClient, INojovAPIClient } from '.';
 
 export class NojovAPIClientFactory {
   public create(): INojovAPIClient {
-    return new NojovAPIClient(process.env.REACT_APP_API_ORIGIN);
+    return new NojovAPIClient('https://dev.api.nojov.kou-pg.com');
   }
 }
