7195c768891e32b3bbc88a4a37c0cb08e8c7dc4e
kou-pg-0131 <kou.pg.0131@gmail.com>
2021-02-14 16:53:56 +0900

    vercel に移行

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -5,14 +5,11 @@ data aws_route53_zone main {
 
 resource aws_route53_record main {
   zone_id = data.aws_route53_zone.main.id
-  name    = local.domain
-  type    = "A"
+  name    = "nojov"
+  type    = "CNAME"
 
-  alias {
-    name                   = aws_cloudfront_distribution.deployment.domain_name
-    zone_id                = aws_cloudfront_distribution.deployment.hosted_zone_id
-    evaluate_target_health = false
-  }
+  records = ["cname.vercel-dns.com"]
+  ttl     = 60
 }
 
 resource aws_route53_record certificate_validation {
