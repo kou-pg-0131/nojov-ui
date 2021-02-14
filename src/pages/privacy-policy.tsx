7f62581e51fb1ec6fe8271b236269f37a7bc58fe
kou-pg-0131 <kou.pg.0131@gmail.com>
2021-02-15 08:35:14 +0900

    privacy-policy をリファクタ

--- src/pages/privacy-policy.tsx
+++ src/pages/privacy-policy.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import { Box, Typography } from '@material-ui/core';
-import { makeStyles, createStyles } from '@material-ui/core/styles';
+import { Typography } from '@material-ui/core';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import { Layout } from '../layout';
 import { PrivacyPolicyItem } from '../components';
 
-const useStyles = makeStyles(() =>
+const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     pageTitle: {
       fontSize: 36,
+      marginBottom: theme.spacing(4),
     },
   })
 );
@@ -15,23 +16,43 @@ const useStyles = makeStyles(() =>
 const PrivacyPolicyPage: React.FC = () => {
   const classes = useStyles();
 
+  const items: { title: string; content: React.ReactNode; }[] = [
+    {
+      title: '免責事項',
+      content: (
+        <>
+          当サイトのコンテンツ・情報につきまして、可能な限り正確な情報を掲載するよう努めておりますが、誤情報が入り込んだり、情報が古くなっていることもございます。当サイトに掲載された内容によって生じた損害等の一切の責任を負いかねますのでご了承ください。
+        </>
+      )
+    },
+    {
+      title: 'アクセス解析ツールについて',
+      content: (
+        <>
+          当サイトでは、 Google によるアクセス解析ツール「 Google アナリティクス」を利用しています。この Google アナリティクスはトラフィックデータの収集のために Cookie を使用しています。このトラフィックデータは匿名で収集されており、個人を特定するものではありません。この機能は Cookie を無効にすることで収集を拒否することが出来ますので、お使いのブラウザの設定をご確認ください。この規約に関して、詳しくは <a href="https://www.google.com/analytics/terms/jp.html" target="_blank" rel="noopener noreferrer">https://www.google.com/analytics/terms/jp.html</a> を参照してください。
+        </>
+      ),
+    },
+    {
+      title: 'プライバシーポリシーの変更について',
+      content: (
+        <>
+          当サイトは、個人情報に関して適用される日本の法令を遵守するとともに、本ポリシーの内容を適宜見直しその改善に努めます。修正された最新のプライバシーポリシーは常に本ページにて開示されます。
+        </>
+      ),
+    },
+  ];
+
   return (
     <Layout title='プライバシーポリシー'>
-      <Box>
-        <Typography variant='h2' className={classes.pageTitle}>プライバシーポリシー</Typography>
+      <Typography variant='h2' className={classes.pageTitle}>プライバシーポリシー</Typography>
 
-        <PrivacyPolicyItem title='免責事項'>
-          当サイトのコンテンツ・情報につきまして、可能な限り正確な情報を掲載するよう努めておりますが、誤情報が入り込んだり、情報が古くなっていることもございます。当サイトに掲載された内容によって生じた損害等の一切の責任を負いかねますのでご了承ください。
+      {items.map(item => (
+        <PrivacyPolicyItem key={item.title} title={item.title}>
+          {item.content}
         </PrivacyPolicyItem>
+      ))}
 
-        <PrivacyPolicyItem title='アクセス解析ツールについて'>
-          当サイトでは、 Google によるアクセス解析ツール「 Google アナリティクス」を利用しています。この Google アナリティクスはトラフィックデータの収集のために Cookie を使用しています。このトラフィックデータは匿名で収集されており、個人を特定するものではありません。この機能は Cookie を無効にすることで収集を拒否することが出来ますので、お使いのブラウザの設定をご確認ください。この規約に関して、詳しくは <a href="https://www.google.com/analytics/terms/jp.html" target="_blank" rel="noopener noreferrer">https://www.google.com/analytics/terms/jp.html</a> を参照してください。
-        </PrivacyPolicyItem>
-
-        <PrivacyPolicyItem title='プライバシーポリシーの変更について'>
-          当サイトは、個人情報に関して適用される日本の法令を遵守するとともに、本ポリシーの内容を適宜見直しその改善に努めます。修正された最新のプライバシーポリシーは常に本ページにて開示されます。
-        </PrivacyPolicyItem>
-      </Box>
     </Layout>
   );
 };
