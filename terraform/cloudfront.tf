f3862907ecde422b4e48ee5ace14a5271e8ceae0
kou-pg-0131 <kou.pg.0131@gmail.com>
2021-03-26 17:16:36 +0900

    リダイレクト用の CloudFront Distribution を作成

---
+++ terraform/cloudfront.tf
@@ -0,0 +1,53 @@
+resource "aws_cloudfront_distribution" "redirect" {
+  enabled = true
+
+  origin {
+    origin_id   = aws_s3_bucket.redirect.id
+    domain_name = "${aws_s3_bucket.redirect.id}.s3-website-us-east-1.amazonaws.com"
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1.2", ]
+    }
+  }
+
+  aliases      = ["nojov.net"]
+  http_version = "http2"
+
+  default_cache_behavior {
+    allowed_methods        = ["GET", "HEAD", ]
+    cached_methods         = ["GET", "HEAD", ]
+    compress               = true
+    default_ttl            = 3600
+    max_ttl                = 86400
+    min_ttl                = 0
+    target_origin_id       = aws_s3_bucket.redirect.id
+    viewer_protocol_policy = "allow-all"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+
+      headers = []
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      locations        = []
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn            = aws_acm_certificate.nojov_net.arn
+    cloudfront_default_certificate = false
+    minimum_protocol_version       = "TLSv1.2_2019"
+    ssl_support_method             = "sni-only"
+  }
+}
--- terraform/route53.tf
+++ terraform/route53.tf
@@ -32,3 +32,20 @@ resource "aws_route53_record" "nojov_net_certificate_validation" {
   records = [aws_acm_certificate.nojov_net.domain_validation_options.*.resource_record_value[0]]
   ttl     = 300
 }
+
+data "aws_route53_zone" "redirect" {
+  name         = "nojov.net"
+  private_zone = false
+}
+
+resource "aws_route53_record" "redirect" {
+  zone_id = data.aws_route53_zone.redirect.id
+  name    = "nojov.net"
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.redirect.domain_name
+    zone_id                = aws_cloudfront_distribution.redirect.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
