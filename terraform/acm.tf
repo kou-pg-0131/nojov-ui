e6b26691cc946e626f3c71f117a70e86efbe700a
kou-pg-0131 <kou.pg.0131@gmail.com>
2021-03-26 16:20:55 +0900

    nojov.net の証明書を発行

--- terraform/acm.tf
+++ terraform/acm.tf
@@ -1,10 +1,21 @@
-resource aws_acm_certificate main {
+resource "aws_acm_certificate" "main" {
   domain_name       = local.domain
   validation_method = "DNS"
   tags              = { Name = local.prefix }
 }
 
-resource aws_acm_certificate_validation main {
+resource "aws_acm_certificate_validation" "main" {
   certificate_arn         = aws_acm_certificate.main.arn
   validation_record_fqdns = [aws_route53_record.certificate_validation.fqdn]
+}
+
+resource "aws_acm_certificate" "nojov_net" {
+  domain_name       = "nojov.net"
+  validation_method = "DNS"
+  tags              = { Name = "${local.prefix}-net" }
+}
+
+resource "aws_acm_certificate_validation" "nojov_net" {
+  certificate_arn         = aws_acm_certificate.nojov_net.arn
+  validation_record_fqdns = [aws_route53_record.nojov_net_certificate_validation.fqdn]
 }
--- terraform/route53.tf
+++ terraform/route53.tf
@@ -1,9 +1,9 @@
-data aws_route53_zone main {
+data "aws_route53_zone" "main" {
   name         = local.zone_name
   private_zone = false
 }
 
-resource aws_route53_record main {
+resource "aws_route53_record" "main" {
   zone_id = data.aws_route53_zone.main.id
   name    = "nojov"
   type    = "CNAME"
@@ -12,10 +12,23 @@ resource aws_route53_record main {
   ttl     = 60
 }
 
-resource aws_route53_record certificate_validation {
+resource "aws_route53_record" "certificate_validation" {
   zone_id = data.aws_route53_zone.main.zone_id
   name    = aws_acm_certificate.main.domain_validation_options.*.resource_record_name[0]
   type    = aws_acm_certificate.main.domain_validation_options.*.resource_record_type[0]
   records = [aws_acm_certificate.main.domain_validation_options.*.resource_record_value[0]]
+  ttl     = 300
+}
+
+data "aws_route53_zone" "nojov_net" {
+  name         = "nojov.net"
+  private_zone = false
+}
+
+resource "aws_route53_record" "nojov_net_certificate_validation" {
+  zone_id = data.aws_route53_zone.nojov_net.zone_id
+  name    = aws_acm_certificate.nojov_net.domain_validation_options.*.resource_record_name[0]
+  type    = aws_acm_certificate.nojov_net.domain_validation_options.*.resource_record_type[0]
+  records = [aws_acm_certificate.nojov_net.domain_validation_options.*.resource_record_value[0]]
   ttl     = 300
 }
