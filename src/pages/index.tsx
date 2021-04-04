f64543cdc11d85e3b57499bbb026e703aa57ef23
kou-pg-0131 <kou.pg.0131@gmail.com>
2021-04-04 13:59:54 +0900

    求人サイトを選択できるようにした

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,45 +3,31 @@ import { Box } from '@material-ui/core';
 import { format } from 'date-fns';
 import { useWebsites } from '../contexts';
 import { Layout } from '../layout';
-import { Loading, JobsTable, JobsBarChart, Checkbox } from '../components';
-import { Job } from '../domain';
+import { Loading, JobsTable, JobsBarChart, Checkbox, WebsitesSelect } from '../components';
+import { Job, Website } from '../domain';
 
 const Home: React.FC = () => {
-  // const [website, setWebsite] = useState<'all' | Website>('all');
   const [sort, setSort] = useState<boolean>(false);
+  const [selectedWebsite, setSelectedWebsite] = useState<Website>();
   const { websites, updatedAt } = useWebsites();
 
-  const jobs: Job[] = websites?.reduce((result, current) => {
-    return [...result, ...current.jobs];
-  }, []);
+  const jobs: Job[] = (() => {
+    if (selectedWebsite) {
+      return selectedWebsite.jobs;
+    }
 
-  // const handleChangeWebsite = (website: 'all' | Website): void => setWebsite(website);
-  //
-  // const websites: Website[] = jobs ? JobsAggregater.getWebsites(jobs) : [];
+    return websites?.reduce((result, current) => {
+      return [...result, ...current.jobs];
+    }, []);
+  })();
+
+  const handleChangeWebsite = (website?: Website): void => {
+    setSelectedWebsite(website);
+  };
 
   const handleChangeSort = (checked: boolean) => {
     setSort(checked);
   };
-
-  // const filteredJobs: Job[] = (() => {
-  //   if (!jobs) return [];
-  //   if (website === 'all') return jobs;
-  //   return JobsAggregater.filterByWebsite(jobs, website);
-  // })();
-  //
-  // const barChartItems = JobsAggregater.sum(filteredJobs).map(job => (
-  //   { language: job.language, count: job.count, color: languageToColor(job.language) }
-  // ));
-  //
-  // const tableItems = (() => {
-  //   if (website === 'all') return JobsAggregater.sum(filteredJobs);
-  //
-  //   return filteredJobs.map(job => ({
-  //     language: job.language,
-  //     count: job.count,
-  //     website: { name: job.website.name, href: job.search_url },
-  //   }));
-  // })();
 
   return (
     <Layout>
@@ -49,7 +35,7 @@ const Home: React.FC = () => {
       {websites && jobs && (
         <>
           <Box style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-end' }}>
-        {/*}<WebsitesSelect onChange={handleChangeWebsite} websites={websites}/>*/}
+          <WebsitesSelect selected={selectedWebsite} onChange={handleChangeWebsite} websites={websites}/>
           <Checkbox label='求人数の多い順に並び替え' onChange={handleChangeSort}/>
           <small>
             最終更新日時: {updatedAt && <time dateTime={updatedAt.toISOString()}>{format(updatedAt, 'yyyy/MM/dd HH:mm')}</time>}
