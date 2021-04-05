dbd68d0b948e4737c8a86c272a3fc6f00fd26266
kou-pg-0131 <kou.pg.0131@gmail.com>
2021-04-05 21:03:38 +0900

    dev を削除

--- src/infrastructures/nojovAPIClient.ts
+++ src/infrastructures/nojovAPIClient.ts
@@ -19,7 +19,7 @@ export class NojovAPIClient {
 
   public async getWebsitesPerYear(): Promise<{ updated_at: Date; websites: Website[] }[]> {
     const from = qs.stringify({ from: addDays(subYears(new Date(), 1), 1).toISOString() });
-    const resp = await this.httpClient.get<{ websites: Website[]; updated_at: string; }[]>(`https://dev.api.nojov.kou-pg.com/v1/websites?${from}`);
+    const resp = await this.httpClient.get<{ websites: Website[]; updated_at: string; }[]>(`https://api.nojov.kou-pg.com/v1/websites?${from}`);
     return resp.map(item => ({
       updated_at: new Date(item.updated_at),
       websites: item.websites,
