3a032f4f4ff89e52f6f2208dc6e5d7bd3b2acb59
kou-pg-0131 <kou.pg.0131@gmail.com>
2021-04-05 19:51:43 +0900

    タブを表示

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Box } from '@material-ui/core';
+import { Box, Tabs, Tab, Paper } from '@material-ui/core';
 import { format } from 'date-fns';
 import { useWebsites } from '../contexts';
 import { Layout } from '../layout';
@@ -7,6 +7,7 @@ import { Loading, JobsTable, JobsBarChart, Checkbox, WebsitesSelect } from '../c
 import { Job, Website } from '../domain';
 
 const Home: React.FC = () => {
+  const [currentTab, setCurrentTab] = useState<string>('daily');
   const [sort, setSort] = useState<boolean>(false);
   const [selectedWebsite, setSelectedWebsite] = useState<Website>();
   const { websites, updatedAt } = useWebsites();
@@ -29,33 +30,52 @@ const Home: React.FC = () => {
     setSort(checked);
   };
 
+  const handleChangeTab = (_: React.ChangeEvent<unknown>, value: string) => {
+    setCurrentTab(value);
+  };
+
   return (
     <Layout>
       {!websites && <Loading/>}
       {websites && jobs && (
         <>
           <Box style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-end' }}>
-          <WebsitesSelect selected={selectedWebsite} onChange={handleChangeWebsite} websites={websites}/>
-          <Checkbox
-            label='求人数の多い順に並び替え'
-            labelPlacement='start'
-            checked={sort}
-            onChange={handleChangeSort}
-          />
-          <small>
-            最終更新日時: {updatedAt && <time dateTime={updatedAt.toISOString()}>{format(updatedAt, 'yyyy/MM/dd HH:mm')}</time>}
-          </small>
+            <WebsitesSelect selected={selectedWebsite} onChange={handleChangeWebsite} websites={websites}/>
+            <Checkbox
+              label='求人数の多い順に並び替え'
+              labelPlacement='start'
+              checked={sort}
+              onChange={handleChangeSort}
+            />
+            <small>
+              最終更新日時: {updatedAt && <time dateTime={updatedAt.toISOString()}>{format(updatedAt, 'yyyy/MM/dd HH:mm')}</time>}
+            </small>
           </Box>
 
-          <JobsBarChart
-            jobs={jobs}
-            sort={sort}
-          />
+          <Paper>
+            <Tabs
+              value={currentTab}
+              variant='fullWidth'
+              indicatorColor='primary'
+              textColor='primary'
+              onChange={handleChangeTab}
+            >
+              <Tab label='今日' value='daily'/>
+              <Tab label='月間' value='monthly'/>
+            </Tabs>
+          </Paper>
 
-          <JobsTable
-            website={selectedWebsite}
-            jobs={jobs}
-          />
+          <Box hidden={currentTab !== 'daily'}>
+            <JobsBarChart
+              jobs={jobs}
+              sort={sort}
+            />
+
+            <JobsTable
+              website={selectedWebsite}
+              jobs={jobs}
+            />
+          </Box>
         </>
       )}
     </Layout>
