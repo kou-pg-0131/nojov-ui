5a33ea3d61d5219f6c7e7318bcc45cbb16534825
kou-pg-0131 <kou.pg.0131@gmail.com>
2021-04-17 14:18:03 +0900

    getLatestWebsites() を削除

--- src/lib/nojovAPIClient.ts
+++ src/lib/nojovAPIClient.ts
@@ -6,14 +6,6 @@ export class NojovAPIClient {
     private httpClient: IHttpClient = new HttpClient(),
   ) {}
 
-  public async getLatestWebsites(): Promise<{ updated_at: Date; websites: Website[] }> {
-    const resp = await this.httpClient.get<{ websites: Website[]; updated_at: string; }>('https://api.nojov.kou-pg.com/v1/websites/latest');
-    return {
-      updated_at: new Date(resp.updated_at),
-      websites: resp.websites,
-    };
-  }
-
   public async getWebsites(): Promise<{ updated_at: Date; websites: Website[] }[]> {
     const resp = await this.httpClient.get<{ websites: Website[]; updated_at: string; }[]>(`https://api.nojov.kou-pg.com/v1/websites`);
     return resp.map(item => ({
