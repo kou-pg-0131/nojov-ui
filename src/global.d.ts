00038fc7e6bd9b3b943f89725f2ac4ad4170ecaf
kou-pg-0131 <kou.pg.0131@gmail.com>
2021-04-17 14:24:15 +0900

    NEXT_PUBLIC_API_ORIGIN 環境変数を削除

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,7 +1,6 @@
 declare namespace NodeJS {
   interface ProcessEnv {
     readonly NEXT_PUBLIC_APP_STAGE?: 'prod';
-    readonly NEXT_PUBLIC_API_ORIGIN: string;
     readonly NEXT_PUBLIC_GA_ID: string;
   }
 }
