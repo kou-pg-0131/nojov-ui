20f754f55dd7b5784f2b8d93066c01568705a265
kou-pg-0131 <kou.pg.0131@gmail.com>
2021-04-17 14:18:21 +0900

    ` -> '

--- src/lib/nojovAPIClient.ts
+++ src/lib/nojovAPIClient.ts
@@ -7,7 +7,7 @@ export class NojovAPIClient {
   ) {}
 
   public async getWebsites(): Promise<{ updated_at: Date; websites: Website[] }[]> {
-    const resp = await this.httpClient.get<{ websites: Website[]; updated_at: string; }[]>(`https://api.nojov.kou-pg.com/v1/websites`);
+    const resp = await this.httpClient.get<{ websites: Website[]; updated_at: string; }[]>('https://api.nojov.kou-pg.com/v1/websites');
     return resp.map(item => ({
       updated_at: new Date(item.updated_at),
       websites: item.websites,
