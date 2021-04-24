95f73357110f5b117aeae059a27c8a962f9bdec7
kou-pg-0131 <kou.pg.0131@gmail.com>
2021-04-24 15:22:09 +0900

    Website に search_urls フィールドを追加、 job から search_url フィールドを削除

--- ui/src/domain/website.ts
+++ ui/src/domain/website.ts
@@ -1,7 +1,13 @@
-import { Job } from '.';
+import { Job, Language } from '.';
 
 export type Website = {
   name: string;
   url: string;
+  search_urls: SearchUrl[];
   jobs: Job[];
+};
+
+export type SearchUrl = {
+  language: Language;
+  url: string;
 };
